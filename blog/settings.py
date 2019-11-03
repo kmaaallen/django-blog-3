143d97882567ec0fb489073e560ddc61064bedf9
Kirsten <kmaaallen@gmail.com>
2019-11-03 20:17:47 +0000

    moved static folder out of posts directory

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-test-app-ka.heroku.com']
+ALLOWED_HOSTS = ['blog-test-app-ka.herokuapp.com']
 
 
 # Application definition
